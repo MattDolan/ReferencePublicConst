a59980f628ac2ebf5e7be19332b568463cd28b0b
MattDolan <matt.r.dolan@gmail.com>
2016-01-10 09:03:01 -0700

    This shows how a const variable is propagated by the compiler

---
+++ CallHere/Callhere.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using DeclareHere;
+
+namespace CallHere
+{
+    public partial class Callhere : Form
+    {
+        public Callhere()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(DeclareHere.DeclareHere.szValue);
+        }
+    }
+}
